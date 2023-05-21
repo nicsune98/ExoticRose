f35aefa846f6b18b89ccfc1056f9c2d9906ed325
nicsune98 <wcuascota123@gmail.com>
2023-05-20 20:20:55 -0500

    Agregar archivos de proyecto.

---
+++ ExoticRose/Invernaderos.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExoticRose
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Invernaderos : ContentPage
+	{
+		public Invernaderos ()
+		{
+			InitializeComponent ();
+            pkInvernadero.Items.Add("Invernadero 1");
+            pkInvernadero.Items.Add("Invernadero 2");
+        }
+
+        private void pkInvernadero_SelectedIndexChanged(object sender, EventArgs e)
+        {
+			
+        }
+    }
+}
---
+++ ExoticRose/Menu.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExoticRose
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Menu : ContentPage
+	{
+		public Menu ()
+		{
+			InitializeComponent ();
+		}
+
+        private void btInvernadero_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Invernaderos());
+        }
+
+        private void btMapa_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btProceso_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btGraficos_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
